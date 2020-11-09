92ba1081ff8c301383b804b2642709a9dcc64570
Karabo Seipelo <karabo.seipelo@wintechnologies.net>
2020-11-09 20:14:09 +0200

    Maroon

--- src/components/Tag/Tag.js
+++ src/components/Tag/Tag.js
@@ -69,7 +69,7 @@ Tag.proptypes = {
 
 Tag.defaultProps = {
     boxShadow: true,
-    backgroundColor: "blue",
+    backgroundColor: "Maroon",
     fontColor: "white",
     href: null,
     header: false,
